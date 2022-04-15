9d2483267ea15b50f0135199b3f6efc81750f48b
Pripii <paniboleti@gmail.com>
2022-04-14 21:04:46 -0300

    Estudos java

---
+++ aula-java/src/aula20220414/AnatomiaDasClasses.java
@@ -0,0 +1,35 @@
+package aula20220414;
+
+
+public class AnatomiaDasClasses {
+public static void main(String[] args) {
+	System.out.println("Olá Mundo!!! xD");
+	
+	//Como NÃO declarar uma variável
+	int numero&um = 1; //Os únicos símbolos permitidos são _ e $
+	int 1numero = 1;    //Uma variável não pode começar com númerico
+	int numero um = 1; //Não pode ter espaço no nome da variável
+	int long = 1; //long faz parte das palavras reservadas da linguagem
+
+	 // Declaração válida de veriáveis
+	int numero$um = 1;
+	int numero1 = 1;
+	int numeroUm = 1;
+	int longo = 1;
+	
+	//Métodos e variáveis
+	// Estrutura
+
+	TipoRetorno NomeObjetivoNoInfinitivo Parametro(s)
+
+	//Exemplo
+
+	int somar (int numeroUm, int numero2)
+
+	String formatarCep (long cep)
+	
+	//EXERCITAR OS METODOS
+	//ALT +SHIFT+r - CORRIGE TODAS AS PAÇAVRAS
+	//final a variável fica constante e não pode ser reatribuida
+}
+}
