0a96603a0827cec1c2a9cdf95d254c50cf970c97
Pripii <paniboleti@gmail.com>
2022-04-21 10:39:15 -0300

    InserindoVariáveis

---
+++ aula-java/src/praticando/Main.java
@@ -0,0 +1,14 @@
+package praticando;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Calculadora
+        System.out.println("Calculadora");
+        Calculadora.soma(7, 21);
+        Calculadora.subtracao(95, 34);
+        Calculadora.multiplicacao(11, 9);
+        Calculadora.divisao(40, 2);
+	}
+}
