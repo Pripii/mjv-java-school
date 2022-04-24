1ee0c543affeb442289a33d126f3f8cf9f6aa03e
Pripii <paniboleti@gmail.com>
2022-04-24 15:51:15 -0300

    Add set servicos

--- agua-luz-primeiro-case.java/src/main/java/Agendamento.java
+++ agua-luz-primeiro-case.java/src/main/java/Agendamento.java
@@ -1,4 +1,13 @@
 
 public class Agendamento {
 
+	 public static void main(String[] args) {
+		 Servicos service = new Servicos ();
+		 service.setProtocolo("");
+		 service.setTipo("");
+		 service.setValor("");
+		 service.setData("");
+		 service.setHora("");
+		 
+}
 }
