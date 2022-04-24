fcab6a23f8ca818699a9452c1ea133a3e22d7580
Pripii <paniboleti@gmail.com>
2022-04-24 15:29:29 -0300

    Add getters & setters
    Add G&S em pessoa, endereco e servicos

--- agua-luz-primeiro-case.java/src/main/java/Pessoa.java
+++ agua-luz-primeiro-case.java/src/main/java/Pessoa.java
@@ -5,5 +5,30 @@ public class Pessoa {
 	private Endereco endereco;
 	private Servicos servicos;
 	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	public Servicos getServicos() {
+		return servicos;
+	}
+	public void setServicos(Servicos servicos) {
+		this.servicos = servicos;
+	}
+	
 	
 }
--- agua-luz-primeiro-case.java/src/main/java/Servicos.java
+++ agua-luz-primeiro-case.java/src/main/java/Servicos.java
@@ -7,4 +7,35 @@ public class Servicos {
 	private Double valor;
 	private LocalDate data;
 	private LocalTime hora;
+	
+	public String getProtocolo() {
+		return protocolo;
+	}
+	public void setProtocolo(String protocolo) {
+		this.protocolo = protocolo;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	public LocalTime getHora() {
+		return hora;
+	}
+	public void setHora(LocalTime hora) {
+		this.hora = hora;
+	}
 }
