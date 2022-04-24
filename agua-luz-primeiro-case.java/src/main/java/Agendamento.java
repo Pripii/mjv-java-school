5d77dc1c0768c0f0daf5881c48327f012bdb5549
Pripii <paniboleti@gmail.com>
2022-04-24 16:42:58 -0300

    add set pessoa e endereço

--- agua-luz-primeiro-case.java/src/main/java/Agendamento.java
+++ agua-luz-primeiro-case.java/src/main/java/Agendamento.java
@@ -1,13 +1,33 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Agendamento {
 
 	 public static void main(String[] args) {
-		 Servicos service = new Servicos ();
-		 service.setProtocolo("");
-		 service.setTipo("");
-		 service.setValor("");
-		 service.setData("");
-		 service.setHora("");
+		 
+		 Pessoa client = new Pessoa();
+		 client.setNome("Priscilla Aniboleti");
+		 client.setCpf("797.979.797.-97");
+		 client.setEndereco(address);
+		 client.setServicos(service);
+		 
+		
+		 Endereco address = new Endereco();
+		 address.setLogadouro("Rua Caldeirão Furado");
+		 address.setComplemento("casa 07");
+		 address.setBairro("Beco Diagonal");
+		 address.setCidade("Londres");
+		 address.setCep("7997");
+		 address.setEstado("Grã-Bretanha");
+		 
+		 
+		 Servicos service = new Servicos();
+		 service.setProtocolo("97979797");
+		 service.setTipo("Água");
+		 service.setValor(97.97);
+		 service.setData(LocalDate.now());
+		 service.setHora(LocalTime.now());
+		 // OU Servicos service = new Servicos ("97979797", "Água", 97.97, LocalDate.now(), LocalTime.now())
 		 
 }
 }
