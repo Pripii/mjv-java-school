9da7dc66aabff43ab248b535b2141beb19a20e16
Pripii <paniboleti@gmail.com>
2022-04-30 18:57:24 -0300

    Add LocalDate e Time.of

--- agua-luz-primeiro-case.java/src/main/java/Util/Agendamento.java
+++ agua-luz-primeiro-case.java/src/main/java/Util/Agendamento.java
@@ -1,5 +1,10 @@
+package Util;
 import java.time.LocalDate;
 import java.time.LocalTime;
+
+import model.Endereco;
+import model.Pessoa;
+import model.Servicos;
 
 public class Agendamento {
 
@@ -18,8 +23,8 @@ public class Agendamento {
 		 service.setProtocolo("97979797");
 		 service.setTipo("Água");
 		 service.setValor(97.97);
-		 service.setData(LocalDate.now());
-		 service.setHora(LocalTime.now());
+		 service.setData(LocalDate.of(2022, 05, 03));
+		 service.setHora(LocalTime.of(17, 00));
 		 // OU Servicos service = new Servicos ("97979797", "Água", 97.97, LocalDate.now(), LocalTime.now())
 		 
 		 Pessoa client = new Pessoa();
---
+++ agua-luz-primeiro-case.java/src/main/java/Util/Notificacao.java
@@ -0,0 +1,17 @@
+package Util;
+import model.Pessoa;
+
+public class Notificacao {
+	public void showMessage(Pessoa client) {
+		
+		System.out.println("Senhor(a) " + client.getNome() + " cpf de número " + client.getCpf() + ", informamos que conforme contrato com protocolo \nde número " + client.getServicos().getProtocolo()+ " está agendado para a data " + client.getServicos().getData() +" e para a hora "+ client.getServicos().getHora() +"h a instalação \ndo serviço de " + client.getServicos().getTipo() + " , com taxa de valor R$" + client.getServicos().getValor().toString().replace(".","," ) + " em sua residência localizada no endereço abaixo:");
+
+		StringBuilder stringbuilder = new StringBuilder();	
+		stringbuilder.append("\nLogradouro: " + client.getEndereco().getLogadouro());
+		stringbuilder.append("\nComplemento: " + client.getEndereco().getComplemento());
+		stringbuilder.append("\nBairro: " + client.getEndereco().getBairro());
+		stringbuilder.append("\nCidade: " + client.getEndereco().getCidade() + " / " + client.getEndereco().getEstado());
+		stringbuilder.append("\nCep: " + client.getEndereco().getCep());
+		System.out.println(stringbuilder.toString());
+	}
+}
