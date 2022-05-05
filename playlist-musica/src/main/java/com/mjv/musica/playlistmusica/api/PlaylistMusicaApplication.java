04fa6ca490c01c6c48435f1892be2ce20852258c
Pripii <paniboleti@gmail.com>
2022-05-05 11:26:35 -0300

    Criando spring boot
    Criando spring boot do projeto de playlist de musicas

---
+++ playlist-musica/src/main/java/com/mjv/musica/playlistmusica/api/PlaylistMusicaApplication.java
@@ -0,0 +1,13 @@
+package com.mjv.musica.playlistmusica.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistMusicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistMusicaApplication.class, args);
+	}
+
+}
