2887a04883c30a88c89a9e0637d818c4684d8d3f
Pripii <paniboleti@gmail.com>
2022-05-13 20:27:41 -0300

    spring initialize
    Criando projeto final

---
+++ music-soul/src/test/java/com/mjv/musicsoul/MusicSoulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjv.musicsoul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicSoulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
